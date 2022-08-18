d52797b17d90dbcee3b902bb691ca6922c4d2b47
NischithaSJ <nischitha.sj@numocity.com>
2022-08-18 13:13:44 +0530

    new changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const sayHi = (req, res) => {
+  res.send("Hi!");
+};
+
+app.get("/", sayHi);
+
+app.post("/add", (req, res) => {
+  const { a, b } = req.body;
+  res.send(`The sum is: ${a + b}`);
+});
+
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000.`);
+});
